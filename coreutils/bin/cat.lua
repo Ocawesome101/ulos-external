e4ef4cca277c4c161c2650b57e1d017148988f3d
Ocawesome101 <ocawesome101@gmail.com>
2021-04-30 00:33:31 -0400

    polish up cat a bit

--- coreutils/bin/cat.lua
+++ coreutils/bin/cat.lua
@@ -2,8 +2,28 @@
 
 local args, opts = require("argutil").parse(...)
 
+if opts.help then
+  io.stderr:write([[
+usage: cat FILE1 FILE2 ...
+Concatenate FILE(s) to standard output.  With no
+FILE, or where FILE is -, read standard input.
+]]
+  os.exit(0)
+end
+
+if #args == 0 then
+  args[1] = "-"
+end
+
 for i=1, #args, 1 do
-  local handle, err = io.open(require("path").canonical(args[i]), "r")
+  local handle, err
+
+  if args[i] == "-" then
+    handle = io.input()
+  else
+    handle, err = io.open(require("path").canonical(args[i]), "r")
+  end
+  
   if not handle then
     io.stderr:write("cat: cannot open '", args[i], "': ", err)
     os.exit(1)
