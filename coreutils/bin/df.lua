c337eae062f1c747bea10b0597e1e6def7805a27
ocawesome101 <ocawesome101@gmail.com>
2021-05-22 17:09:14 -0400

    implemented df, finally

--- coreutils/bin/df.lua
+++ coreutils/bin/df.lua
@@ -1,7 +1,43 @@
 -- df --
 
+local path = require("path")
+local size = require("size")
 local filesystem = require("filesystem")
 
 local args, opts = require("argutil").parse(...)
 
-local mounts = fs.mounts()
+local fscpath = "/sys/components/by-type/filesystem/"
+local files = filesystem.list(fscpath)
+
+table.sort(files)
+
+print("      fs     name    total     used     free")
+
+local function readFile(f)
+  local handle = assert(io.open(f, "r"))
+  local data = handle:read("a")
+  handle:close()
+
+  return data
+end
+
+local function printInfo(fs)
+  local addr = readFile(fs.."/address"):sub(1, 8)
+  local name = readFile(fs.."/label")
+  local used = tonumber(readFile(fs.."/spaceUsed"))
+  local total = tonumber(readFile(fs.."/spaceTotal"))
+
+  local free = total - used
+
+  if opts.h then
+    used = size.format(used)
+    free = size.format(free)
+    total = size.format(total)
+  end
+
+  print(string.format("%8s %8s %8s %8s %8s", addr, name, total, used, free))
+end
+
+for i, file in ipairs(files) do
+  printInfo(path.concat(fscpath, file))
+end
