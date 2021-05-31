d51b27bb85ba9920e781fc1b205b138f1efcc15e
ocawesome101 <ocawesome101@gmail.com>
2021-05-30 22:48:52 -0400

    more tfmt, 1 man page

--- coreutils/bin/tfmt.lua
+++ coreutils/bin/tfmt.lua
@@ -2,16 +2,35 @@
 
 local args, opts = require("argutil").parse(...)
 
+if #args == 0 or opts.help then
+  io.stderr:write([[
+usage: tfmt [options] FILE ...
+Format FILE(s) according to a simple format
+specification.
+
+Options:
+  --wrap=WD   Wrap output text at WD characters.
+
+ULOS Coreutils copyright (c) 2021 Ocawesome101
+under the DSLv2.
+]])
+  os.exit(1)
+end
+
 local colors = {
   bold = "97",
   regular = "39",
-  italic = "92"
+  italic = "36",
+  link = "94"
 }
 
 local patterns = {
   {"%*(%b{})", "bold"},
-  {"_(%b{})", "italic"},
+  {"%$(%b{})", "italic"},
+  {"@(%b{})", "link"}
 }
+
+opts.wrap = tonumber(opts.wrap)
 
 for i=1, #args, 1 do
   local handle, err = io.open(args[i], "r")
@@ -21,6 +40,10 @@ for i=1, #args, 1 do
   end
   local data = handle:read("a")
   handle:close()
+
+  if opts.wrap then
+    -- TODO
+  end
 
   for i=1, #patterns, 1 do
     data = data:gsub(patterns[i][1], function(x)
