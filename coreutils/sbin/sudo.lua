de920b01a679539bc5b5b6407bf3ccf48ccb2c32
ocawesome101 <ocawesome101@gmail.com>
2021-05-31 20:48:45 -0400

    sudo and shutdown manpages, sudo now takes uid as a more standard option

--- coreutils/sbin/sudo.lua
+++ coreutils/sbin/sudo.lua
@@ -6,14 +6,14 @@ local process = require("process")
 local args = table.pack(...)
 
 local uid = 0
-if args[1] and args[1]:match("^uid=%d+$") then
+if args[1] and args[1]:match("^%-%-uid=%d+$") then
   uid = tonumber(args[1]:match("uid=(%d+)")) or 0
   table.remove(args, 1)
 end
 
 if #args == 0 then
   io.stderr:write([[
-sudo: usage: sudo [uid=UID] COMMAND
+sudo: usage: sudo [--uid=UID] COMMAND
 Executes COMMAND as root or the specified UID.
 ]])
   os.exit(1)
