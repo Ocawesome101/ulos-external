cf1fdd1b36be4d4e7d33476d3c054b8eb6050382
Ocawesome101 <ocawesome101@gmail.com>
2021-09-02 10:54:35 -0400

    more work on BSH

--- bsh/bin/bsh.lua
+++ bsh/bin/bsh.lua
@@ -1,11 +1,12 @@
 -- bsh: Bourne Shell --
 
---local process = require("process")
---local readline = require("readline")
+local pipe = require("pipe")
+local process = require("process")
+local readline = require("readline")
 
 local special = "['\" %[%(%$&#|%){}\n;]"
 local function tokenize(text)
-  text = text:gsub("$([a-zA-Z0-9_]+)", os.getenv)
+  text = text:gsub("$([a-zA-Z0-9_]+)", function(x)return os.getenv(x)or""end)
   local tokens = {}
   local idx = 0
   while #text > 0 do
@@ -39,7 +40,7 @@ do
     return self.t[self.i - 1]
   end
   function r:peek(n)
-    return selt.t[self.i+(n or 0)]
+    return self.t[self.i+(n or 0)]
   end
   function r:get_until(c)
     local t={}
@@ -66,8 +67,9 @@ do
 end
 
 local eval
-eval = function(tokens)
-  local simplified = {}
+eval = function(tokens, captureOutput)
+  -- first pass: simplify it all
+  local simplified = {""}
   while true do
     local tok = tokens:pop()
     if not tok then break end
@@ -75,26 +77,50 @@ eval = function(tokens)
       if tokens:peek() == "(" then
         local seq = tokens:get_balanced("(",")")
         seq[#seq] = nil
-        seq = eval(seq)
+        seq = eval(mkrdr(seq), true) or {}
         simplified[#simplified]=simplified[#simplified]..table.concat(seq)
       elseif tokens:peek() == "{" then
         local seq = tokens:get_balanced("{","}")
         seq[#seq]=nil
-        simplified[#simplified]=simplified[#simplified]..table.concat(seq)
+        simplified[#simplified]=simplified[#simplified]..(os.getenv(table.concat(seq))or"")
       else
         simplified[#simplified] = simplified[#simplified] .. tok
       end
     elseif tok == "#" then
-      tokens:get_next("\n")
+      tokens:get_until("\n")
     elseif tok:match("[ |;\n&]") and #simplified[#simplified] > 0 then
       if tok ~= " " and tok ~= "\n" then simplified[#simplified+1] = tok end
       simplified[#simplified + 1] = ""
+    elseif tok == "}" then
+      return nil, "syntax error near unexpected token `}'"
+    elseif tok == ")" then
+      return nil, "syntax error near unexpected token `)'"
     else
       simplified[#simplified] = simplified[#simplified] .. tok
     end
   end
-  return simplified
+  if #simplified == 0 then return end
+  local _cout_pipe
+  if captureOutput then
+    _cout_pipe = pipe.create()
+  end
+  -- second pass: set up command structure
+  local struct = {{command = {}, input = io.stdin,
+    output = (captureOutput and _cout_pipe or io.stdout), err = io.stderr}}
+  local i = 0
+  while i < #simplified do
+    i = i + 1
+    if simplified[i] == ";" then
+      if #struct[#struct].command == 0 then
+        return nil, "syntax error near unexpected token `;'"
+      else
+        struct[#struct+1] = {{command = {}, }}
+      end
+    end
+  end
 end
 
 while true do
+  io.write("$ ")
+  print(eval(mkrdr(tokenize(io.read()))))
 end
